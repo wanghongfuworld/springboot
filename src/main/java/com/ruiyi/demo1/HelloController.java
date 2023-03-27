b2dffcf0ee5510eb69d523f5b4d19a6171c552d5
username <2966849043@qq.com>
2023-03-27 12:15:16 +0800

    SpringBoot01

---
+++ src/main/java/com/ruiyi/demo1/HelloController.java
@@ -0,0 +1,12 @@
+package com.ruiyi.demo1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello,java";
+    }
+}
